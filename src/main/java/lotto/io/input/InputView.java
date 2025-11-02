b4c6eafca760bcb876c0bb054033b852293710d6
Yoonhojoon <dbsghwns1209@gmail.com>
2025-11-02 20:01:07 +0900

    feat(InputView): Input과 OutputView 구현

---
+++ src/main/java/lotto/io/input/InputView.java
@@ -0,0 +1,10 @@
+package lotto.io.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String read(String line) {
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/lotto/io/output/OutputView.java
@@ -0,0 +1,23 @@
+package lotto.io.output;
+
+public class OutputView {
+    public void printPurchasePrice() { System.out.println("구입금액을 입력해 주세요."); }
+
+    public void printPurchaseAmount(int purchaseNum) {System.out.println(purchaseNum + "개를 구매했습니다.");}
+
+    public void printPurchaseRecords() {}
+
+    public void printWinNumber() {System.out.println("당첨 번호를 입력해 주세요.");}
+
+    public void printBonusNumber() {System.out.println("보너스 번호를 입력해 주세요.");}
+
+    public void printWinStatistics() {
+        System.out.println("당첨 통계\n---");
+        printEachMatchStatus();
+    }
+
+    public void printEachMatchStatus(){}
+
+    public void printTotalReturnStatus(int returnRate) {System.out.println("총 수익률은 "+ returnRate  +
+            "%입니다.");}
+}
